d7eb1aebd0f8e6998a21e68e0cdb4bffc90a45b1
vbreddyobulareddy <vbreddy.obulareddy@gmail.com>
2024-03-07 17:50:54 -0500

    Solved useParams issue with Jest  UnitTest using MemoryRoute

--- src/containers/details.test.tsx
+++ src/containers/details.test.tsx
@@ -2,7 +2,7 @@ import { render, screen } from "@testing-library/react";
 import { Provider } from "react-redux";
 import { appStore } from "../store";
 import DetailsContainer from "./details";
-import { MemoryRouter, Route } from "react-router-dom";
+import { MemoryRouter } from "react-router-dom";
 
 describe("TableViewComponent UnitTest", () => {
   test("Render TableViewComponent", () => {
@@ -10,14 +10,12 @@ describe("TableViewComponent UnitTest", () => {
       <>
         <Provider store={appStore}>
           <MemoryRouter initialEntries={["/02M260"]}>
-            <Route path="/:id">
               <DetailsContainer />
-            </Route>
           </MemoryRouter>
         </Provider>
       </>
     );
-    const linkElement = screen.getByTestId("table_view_component");
+    const linkElement = screen.getByTestId("details_snap_component");
     expect(linkElement).toBeInTheDocument();
   });
 });
